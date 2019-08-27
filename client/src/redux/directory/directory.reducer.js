46dc4db2468ef51bb173c6da691793757cfd8058
haruyasu <harukun2002@gmail.com>
2019-08-27 16:00:47 +0900

    update img

--- client/src/redux/directory/directory.reducer.js
+++ client/src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ export const INITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl: 'https://i.ibb.co/HGtjZfK/hats.png',
       id: 1,
       linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl: 'https://i.ibb.co/wrxJbPd/jackets.png',
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://i.ibb.co/T8C0JGn/sneakers.png',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: 'https://i.ibb.co/m63n30f/womens.png',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://i.ibb.co/CVzLynn/mens.png',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
